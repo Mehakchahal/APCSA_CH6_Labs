bb241b805d4610b69dfac47e09a1ce7649f3aac4
mehak.chahal <mehakchahal01@gmail.com>
2018-10-18 08:50:59 -0700

    Lab 6.1 is mostly finished but still need to work on it.

--- src/Sales/Sales.java
+++ src/Sales/Sales.java
@@ -1,8 +1,8 @@
 package Sales;
 /**
- * Name:
- * Date:
- * Period:
+ * Name: Mehak Chahal
+ * Date: October 18, 2018
+ * Period: 1st period
  *
  * Sales.Sales.java
  *
@@ -22,6 +22,7 @@ public class Sales
         Scanner scan = new Scanner(System.in);
 
         //TODO 4) Prompt the user for the number of sales people that they need to enter
+
         //TODO 4) and modify the program to create an appropriately sized array.
 
         
@@ -50,35 +51,66 @@ public class Sales
 
 
         //TODO 1b) Use your average method to print the average sales
-
+        System.out.println("The average sales is: " + average(sales));
 
         //TODO 2b) Use your indexOfMax method to print the index of the best sales person
-        //TODO 2b) and the sales amount for that sales person.
 
+        //TODO 2b) and the sales amount for that sales person.
+        System.out.println("The person with the highest sales is Salesperson: " + indexOfMax(sales));
+        System.out.println("They sold " + sales[indexOfMax(sales)] + " units.");
 
         //TODO 3b) Do the same for the worst sales person
-
-
+        System.out.println("The person with the lowest sales is Salesperson: " + indexOfMin(sales));
+        System.out.println("They sold " + sales[indexOfMin(sales)] + " units.");
 
     }
 
     public static double average(int[] arr) throws IllegalStateException
     {
+        //Check for an array w/ length 0 because I don't want to divide by zero later
+        if(arr.length==0)
+        {
+            throw new IllegalStateException();
+        }
         //TODO 1a) compute and return the average of arr
         //Be sure to use the length instance variable and you may assume that the array is full.
         //If the array does not have at least 1 element in it, throw an IllegalStateException
+        int sum = 0;
+        //loop through the array
+        for(int i = 0; i < arr.length; i++)
+        {
+            sum += arr[i];
+        }
 
-        return 0;
+        return sum/arr.length;
     }
 
     public static int indexOfMax(int[] arr)
     {
         //TODO 2a) find and return the index of the max value in arr
-
-        return 0;
+        int max = Integer.MIN_VALUE;
+        int maxLocation = -1;
+        for (int i = 0; i < arr.length; i++)
+        {
+            if (max < arr[i])
+            {
+                max = arr[i];
+                maxLocation = i;
+            }
+        }
+        return maxLocation;
     }
 
     //TODO 3a) Do the same for an indexOfMin method
-
-
+        int min = Integer.MAX_VALUE;
+        int minLocation = -1;
+        for (int i = 0; );
+        {
+          if (min > arr[i])
+          {
+            min = arr[i];
+            minLocation = i;
+          }
+        }
+        return minLocation;
 }
